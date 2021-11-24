b58b60ff828e686a01a196aca5e46b5d7a34b5ba
Phindokuhle Dlamini <phindokuhle.dlamini@eec.co.sz>
2021-11-24 15:01:57 +0200

    End of Section 9

--- client-app/src/features/activities/details/ActivityDetails.tsx
+++ client-app/src/features/activities/details/ActivityDetails.tsx
@@ -1,42 +1,35 @@
 import { observer } from "mobx-react-lite";
 import React, { useEffect } from "react";
 import { useParams } from "react-router";
-import { Link } from "react-router-dom";
-import { Button, Card, Image } from "semantic-ui-react";
+import { Grid, GridColumn } from "semantic-ui-react";
 import LoadingComponent from "../../../app/layout/loadingComponent";
-import { Activity } from "../../../app/models/activity";
 import { useStore } from "../../../app/stores/store";
+import ActivityDetailedChat from "./ActivityDetailedChat";
+import ActivityDetailedHeader from "./ActivityDetailedHeader";
+import ActivityDetailedInfo from "./ActivityDetailedInfo";
+import ActivityDetailedSidebar from "./ActivityDetailedSidebar";
 
 
 export default observer(function ActivityDetails(){
   const {activityStore} = useStore();
   const {selectedActivity: activity, loadActivity, loadingInitial} = activityStore;  
   const {id} = useParams<{id:string}>();
-  console.log("NO id");
+
   useEffect(()=> {
     if(id) loadActivity(id);
   }, [id, loadActivity]);
   
   if(loadingInitial || !activity) return <LoadingComponent content = 'Loading activity'/>;
     return(
-        <Card>
-        <Image src= {`/assets/categoryImages/${activity.category}.jpg`}/>
-        <Card.Content>
-          <Card.Header>{activity.title}</Card.Header>
-          <Card.Meta>
-            <span>{activity.date}</span>
-          </Card.Meta>
-          <Card.Description>
-            {activity.description}
-          </Card.Description>
-        </Card.Content>
-        <Card.Content extra>
-             <Button.Group width = '2'>
-                 <Button  as={Link} to={`/manage/${activity.id}`} basic color = 'blue' content = 'Edit'/>
-                 <Button  as={Link} to={'/activities'} basic color = 'blue' content = 'Cancel'/>
-
-             </Button.Group>
-        </Card.Content>
-      </Card>  
+       <Grid>
+         <GridColumn width={10}>
+         <ActivityDetailedHeader activity = {activity}/>
+         <ActivityDetailedInfo activity = {activity} />
+         <ActivityDetailedChat/>
+         </GridColumn>
+         <GridColumn width ={6}>
+           <ActivityDetailedSidebar/>
+         </GridColumn>
+       </Grid>
     )
 })
--- client-app/src/index.tsx
+++ client-app/src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './app/layout/styles.css';
 import App from './app/layout/App';
+import 'react-calendar/dist/Calendar.css';
 import 'semantic-ui-css/semantic.min.css'; 
 import reportWebVitals from './reportWebVitals';
 import { store, StoreContext } from './app/stores/store';
